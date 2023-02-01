c1b598e00d2b501bee4ee621ca2887609eac21c0
JoerivanEngelen <joerivanengelen@hotmail.com>
2023-02-01 13:21:49 +0100

    Rename install index and provide better title document

--- docs/conf.py
+++ docs/conf.py
@@ -139,9 +139,9 @@ ignore_text_in_index = True
 latex_documents = [
     ("index", "imod-doc.tex", "iMOD Suite", _stdauthor, "manual", ignore_text_in_index),
     (
-        "viewer_install_index",
+        "install_index",
         "imod-installation.tex",
-        "iMOD Viewer install guide",
+        "iMOD Suite install guide",
         _stdauthor,
         "manual",
         False,
